f48251e0defda25cdc8a17c1520cc6f3a26758f8
michael071788 <michael071788@gmail.com>
2022-12-15 11:46:42 +0800

    Rollback the signup error message response

--- routes/user.js
+++ routes/user.js
@@ -75,7 +75,7 @@ router.post("/signup", async (req, res) => {
       data,
     });
   } catch (error) {
-    res.send(error);
+    res.status(500).send({ message: "Internal Server Error" });
   }
 });
 
