99564161abf5539297e3a139e0f1e2bf83c5a7f5
michael071788 <michael071788@gmail.com>
2023-04-07 17:45:42 +0800

    Initial schema and functionality for counting

--- routes/advert.js
+++ routes/advert.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { AdvertList } = require("../models/advert_list");
 
+// get all advert data
 router.get("/list", async (req, res) => {
   try {
     const advertLists = await AdvertList.find();
@@ -10,6 +11,7 @@ router.get("/list", async (req, res) => {
   }
 });
 
+// upload advert data
 router.post("/", async (req, res) => {
   try {
     const responseData = await new AdvertList({ ...req.body }).save();
@@ -22,8 +24,87 @@ router.post("/", async (req, res) => {
       data,
     });
   } catch (error) {
+    console.log(error);
     res.status(500).send({ message: "Internal Server Error" });
   }
+});
+
+// like advert video
+router.put("/like/:id", (req, res) => {
+  AdvertList.findByIdAndUpdate(
+    req.body.videoId,
+    {
+      $push: { likes: req.params.id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(422).json({ error: err });
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+// unlike advert video
+router.put("/unlike/:id", (req, res) => {
+  AdvertList.findByIdAndUpdate(
+    req.body.videoId,
+    {
+      $pull: { likes: req.params.id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+// watch advert video
+router.put("/watch/:id", (req, res) => {
+  AdvertList.findByIdAndUpdate(
+    req.body.videoId,
+    {
+      $push: { watch: req.params.id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      console.log("success ", result);
+      return res.status(422).json({ error: err });
+    } else {
+      res.status(200).send(result);
+    }
+  });
+});
+
+// share advert video
+router.put("/share/:id", (req, res) => {
+  AdvertList.findByIdAndUpdate(
+    req.body.videoId,
+    {
+      $push: { share: req.params.id },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.status(200).send(result);
+    }
+  });
 });
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,35 @@
 const router = require("express").Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+
+const otp = Math.floor(1000 + Math.random() * 9000); // generate a 4-digit OTP
+
+const sendEmail = (user) => {
+  return new Promise((resolve, reject) => {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.appEmail,
+        pass: process.env.appPassword,
+      },
+      tls: { rejectUnauthorized: false },
+    });
+    const mail_config = {
+      from: process.env.appEmail,
+      to: user,
+      subject: "OTP",
+      text: `Your otp is ${otp}`,
+    };
+    transporter.sendMail(mail_config, function (error, info) {
+      if (error) {
+        console.log("email error", error);
+        return reject({ message: "An error occured" });
+      }
+      return resolve({ message: "Email sent successfully" });
+    });
+  });
+};
 
 router.get("/", async (req, res) => {
   try {
@@ -11,19 +40,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+// find specific user
 router.get("/:id", async (req, res) => {
   const id = req.params.id;
-
-  User.findById(id, function (err, docs) {
-    if (err) {
-      console.log(err);
-      res.send("User not found");
-    } else {
-      console.log("Result : ", docs);
-      res.send(docs);
-    }
-  });
+  try {
+    const userData = await User.findById(id);
+    res.send(userData);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
+
+// update user info
 
 router.patch("/update/:id", async (req, res) => {
   try {
@@ -36,6 +64,46 @@ router.patch("/update/:id", async (req, res) => {
   }
 });
 
+// update user password
+router.patch("/update-password/:id", async (req, res) => {
+  try {
+    const userData = await User.findById(req.params.id);
+
+    const isMatch = await bcrypt.compare(
+      req.body.currentPassword,
+      userData.password
+    );
+
+    if (!isMatch) {
+      // Hash the new password
+      return res.status(401).send({ message: "Invalid Password" });
+    }
+
+    if (!userData) return res.status(401).send({ message: "User not found" });
+
+    if (req.body.newPassword !== req.body.confirmPassword) {
+      return res.status(401).send({ message: "Password not match" });
+    }
+
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcrypt.hash(req.body.newPassword, salt);
+
+    await User.findByIdAndUpdate(userData.id, {
+      $set: {
+        password: hashPassword,
+      },
+    });
+
+    res.status(201).send({
+      message: "successfully",
+      status: res.statusCode,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+    console.log(error);
+  }
+});
+
 router.post("/signup", async (req, res) => {
   try {
     // validate user inputs
@@ -43,13 +111,14 @@ router.post("/signup", async (req, res) => {
     if (error)
       return res.status(400).send({ message: error.details[0].message });
 
-    // validate if the email exists
+    // validate if the email already exists
     const user = await User.findOne({ email: req.body.email });
     if (user)
       return res
         .status(400)
         .send({ message: "User with given email already Exist!" });
 
+    // validate if the phone exists
     const phone = await User.findOne({ phone: req.body.phone });
     if (phone)
       return res
@@ -60,10 +129,11 @@ router.post("/signup", async (req, res) => {
     const salt = await bcrypt.genSalt(Number(process.env.SALT));
     const hashPassword = await bcrypt.hash(req.body.password, salt);
 
-    await new User({ ...req.body, password: hashPassword }).save();
+    await new User({ ...req.body, OTP: otp, password: hashPassword }).save();
 
     let data = new User({
-      name: req.body.name,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
       email: req.body.email,
       phone: req.body.phone,
       password: req.body.password,
@@ -75,8 +145,73 @@ router.post("/signup", async (req, res) => {
       data,
     });
   } catch (error) {
-    res.status(500).send({ message: "Internal Server Error" });
+    res.status(500).send({ message: "Internal server error" });
   }
+});
+
+// get all user tickets
+router.post("/tickets/:id", async (req, res) => {
+  User.findByIdAndUpdate(
+    req.params.id,
+    {
+      $push: {
+        earnedTickets: {
+          ...req.body,
+        },
+      },
+    },
+    {
+      new: true,
+    }
+  ).exec((err, result) => {
+    if (err) {
+      return res.status(422).json({ error: err });
+    } else {
+      res.status(200).send({
+        message: "success",
+        result,
+      });
+    }
+  });
+});
+
+// get user profile image
+router.get("/profile-image/:id", async (req, res) => {
+  const profileImage = await User.findById(req.params.id);
+  res.send(profileImage.profile_image);
+});
+
+router.post("/verify-otp", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return res.status(404).send("User not found.");
+  }
+
+  if (user.OTP === req.body.otp) {
+    user.isVerified = true;
+
+    const token = user.generateAuthToken();
+
+    await user.save();
+
+    return res.status(200).send({
+      message: "Email verified successfully.",
+      token: token,
+      status: res.statusCode,
+      user: user,
+    });
+  } else {
+    return res.status(401).send({ message: "Invalid OTP." });
+  }
+});
+
+router.post("/generate-otp", (req, res) => {
+  const user = req.body.email;
+
+  sendEmail(user)
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
 });
 
 module.exports = router;
